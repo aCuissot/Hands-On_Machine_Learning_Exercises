23e1e90109a958dfc1cffc18a41a56a4ee422c40
aCuissot <harrypotter9752@gmail.com>
2019-05-10 15:52:40 +0200

    ch4 (W)

--- 4_Training_Models/4_Models.py
+++ 4_Training_Models/4_Models.py
@@ -27,3 +27,270 @@ lin_reg.fit(X, y)
 print(lin_reg.intercept_)
 print(lin_reg.coef_)
 lin_reg.predict(X_new)
+
+# Gradient Descente
+print("===== Gradient Descente =====")
+eta = 0.1  # learning rate, important for a quick convegence
+n_iterations = 1000
+m = 100
+theta = np.random.randn(2, 1)  # random initialization
+for iteration in range(n_iterations):
+    gradients = 2 / m * X_b.T.dot(X_b.dot(theta) - y)
+    theta = theta - eta * gradients
+print(theta)
+
+# Stochastic version
+n_epochs = 50
+t0, t1 = 5, 50  # learning schedule hyperparameters
+
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+
+theta = np.random.randn(2, 1)  # random initialization
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index + 1]
+        yi = y[random_index:random_index + 1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+
+print(theta)
+
+from sklearn.linear_model import SGDRegressor
+
+sgd_reg = SGDRegressor(n_iter=50, penalty=None, eta0=0.1)
+sgd_reg.fit(X, y.ravel())
+print(sgd_reg.intercept_)
+print(sgd_reg.coef_)
+
+# Polynomial Regression
+print("===== Polynoiale Regression =====")
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X ** 2 + X + 2 + np.random.randn(m, 1)
+
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+
+poly_features = PolynomialFeatures(degree=2, include_bias=False)
+X_poly = poly_features.fit_transform(X)
+
+print(X[0])
+print(X_poly[0])
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+print(lin_reg.intercept_)
+print(lin_reg.coef_)
+plt.plot(X, y, 'b.')
+
+
+def prediction_model(abscisse):
+    return (abscisse ** 2) * lin_reg.coef_[0][1] + abscisse * lin_reg.coef_[0][0] + lin_reg.intercept_[0]
+
+
+prediction = []
+prediction_abs = []
+for i in range(180):
+    prediction.append(prediction_model(i / 30 - 3))
+    prediction_abs.append(i / 30 - 3)
+plt.plot(prediction_abs, prediction, 'r-')
+plt.show()
+
+# Learning Curves
+print("===== Learning Curves =====")
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train_predict, y_train[:m]))
+        val_errors.append(mean_squared_error(y_val_predict, y_val))
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+
+
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y)
+plt.show()
+
+from sklearn.pipeline import Pipeline
+
+polynomial_regression = Pipeline((
+    ("poly_features", PolynomialFeatures(degree=10, include_bias=False)),
+    ("sgd_reg", LinearRegression()),
+))
+plot_learning_curves(polynomial_regression, X, y)
+plt.show()
+
+# Regularized Linear
+print("===== Regularize Linear =====")
+print("=== Ridge Regression ===")
+from sklearn.linear_model import Ridge
+
+# Using closed form solution equation
+ridge_reg = Ridge(alpha=1, solver="cholesky")
+ridge_reg.fit(X, y)
+print(ridge_reg.predict([[1.5]]))
+# Using stochastic gradient descente
+sgd_reg = SGDRegressor(penalty="l2")
+sgd_reg.fit(X, y.ravel())
+print(sgd_reg.predict([[1.5]]))
+
+print("=== Lasso Regression ===")
+
+from sklearn.linear_model import Lasso
+
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+print(lasso_reg.predict([[1.5]]))
+
+print("=== Elastic Net ===")
+
+from sklearn.linear_model import ElasticNet
+
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+print(elastic_net.predict([[1.5]]))
+
+print("=== Early Stop ===")
+
+from sklearn.base import clone
+
+sgd_reg = SGDRegressor(n_iter=1, warm_start=True, penalty=None,
+                       learning_rate="constant", eta0=0.0005)
+minimum_val_error = float("inf")
+best_epoch = None
+best_model = None
+
+X_train, X_val, y_train, y_val = train_test_split(X[:50], y[:50].ravel(), test_size=0.5, random_state=10)
+
+poly_scaler = Pipeline([
+    ("poly_features", PolynomialFeatures(degree=90, include_bias=False)),
+    ("std_scaler", StandardScaler()),
+])
+X_train_poly_scaled = poly_scaler.fit_transform(X_train)
+X_val_poly_scaled = poly_scaler.transform(X_val)
+
+best_epoch, best_model
+
+for epoch in range(1000):
+    sgd_reg.fit(X_train_poly_scaled, y_train)  # continues where it left off
+    y_val_predict = sgd_reg.predict(X_val_poly_scaled)
+    val_error = mean_squared_error(y_val_predict, y_val)
+    if val_error < minimum_val_error:
+        minimum_val_error = val_error
+        best_epoch = epoch
+        best_model = clone(sgd_reg)
+
+t1a, t1b, t2a, t2b = -1, 3, -1.5, 1.5
+
+# ignoring bias term
+t1s = np.linspace(t1a, t1b, 500)
+t2s = np.linspace(t2a, t2b, 500)
+t1, t2 = np.meshgrid(t1s, t2s)
+T = np.c_[t1.ravel(), t2.ravel()]
+Xr = np.array([[-1, 1], [-0.3, -1], [1, 0.1]])
+yr = 2 * Xr[:, :1] + 0.5 * Xr[:, 1:]
+
+J = (1 / len(Xr) * np.sum((T.dot(Xr.T) - yr.T) ** 2, axis=1)).reshape(t1.shape)
+
+N1 = np.linalg.norm(T, ord=1, axis=1).reshape(t1.shape)
+N2 = np.linalg.norm(T, ord=2, axis=1).reshape(t1.shape)
+
+t_min_idx = np.unravel_index(np.argmin(J), J.shape)
+t1_min, t2_min = t1[t_min_idx], t2[t_min_idx]
+
+t_init = np.array([[0.25], [-1]])
+
+
+def bgd_path(theta, X, y, l1, l2, core=1, eta=0.1, n_iterations=50):
+    path = [theta]
+    for iteration in range(n_iterations):
+        gradients = core * 2 / len(X) * X.T.dot(X.dot(theta) - y) + l1 * np.sign(theta) + 2 * l2 * theta
+        theta = theta - eta * gradients
+        path.append(theta)
+    return np.array(path)
+
+
+plt.figure(figsize=(12, 8))
+for i, N, l1, l2, title in ((0, N1, 0.5, 0, "Lasso"), (1, N2, 0, 0.1, "Ridge")):
+    JR = J + l1 * N1 + l2 * N2 ** 2
+
+    tr_min_idx = np.unravel_index(np.argmin(JR), JR.shape)
+    t1r_min, t2r_min = t1[tr_min_idx], t2[tr_min_idx]
+
+    levelsJ = (np.exp(np.linspace(0, 1, 20)) - 1) * (np.max(J) - np.min(J)) + np.min(J)
+    levelsJR = (np.exp(np.linspace(0, 1, 20)) - 1) * (np.max(JR) - np.min(JR)) + np.min(JR)
+    levelsN = np.linspace(0, np.max(N), 10)
+
+    path_J = bgd_path(t_init, Xr, yr, l1=0, l2=0)
+    path_JR = bgd_path(t_init, Xr, yr, l1, l2)
+    path_N = bgd_path(t_init, Xr, yr, np.sign(l1) / 3, np.sign(l2), core=0)
+
+    plt.subplot(221 + i * 2)
+    plt.grid(True)
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+    plt.contourf(t1, t2, J, levels=levelsJ, alpha=0.9)
+    plt.contour(t1, t2, N, levels=levelsN)
+    plt.plot(path_J[:, 0], path_J[:, 1], "w-o")
+    plt.plot(path_N[:, 0], path_N[:, 1], "y-^")
+    plt.plot(t1_min, t2_min, "rs")
+    plt.title(r"$\ell_{}$ penalty".format(i + 1), fontsize=16)
+    plt.axis([t1a, t1b, t2a, t2b])
+    if i == 1:
+        plt.xlabel(r"$\theta_1$", fontsize=20)
+    plt.ylabel(r"$\theta_2$", fontsize=20, rotation=0)
+
+    plt.subplot(222 + i * 2)
+    plt.grid(True)
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+    plt.contourf(t1, t2, JR, levels=levelsJR, alpha=0.9)
+    plt.plot(path_JR[:, 0], path_JR[:, 1], "w-o")
+    plt.plot(t1r_min, t2r_min, "rs")
+    plt.title(title, fontsize=16)
+    plt.axis([t1a, t1b, t2a, t2b])
+    if i == 1:
+        plt.xlabel(r"$\theta_1$", fontsize=20)
+
+# save_fig("lasso_vs_ridge_plot")
+plt.show()
+
+# Logistic Regression
+print("===== Logistic Regression =====")
+
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+iris = datasets.load_iris()
+
+print(list(iris.keys()))
+X = iris["data"][:, 3:]  # petal width
+y = (iris["target"] == 2).astype(np.int)  # 1 if Iris-Virginica, else 0
+log_reg = LogisticRegression()
+log_reg.fit(X, y)
+X_new = np.linspace(0, 3, 1000).reshape(-1, 1)
+y_proba = log_reg.predict_proba(X_new)
+plt.plot(X_new, y_proba[:, 1], "g-", label="Iris-Virginica")
+plt.plot(X_new, y_proba[:, 0], "b--", label="Not Iris-Virginica")
+plt.legend()
+plt.show()
+print(log_reg.predict([[1.7], [1.5]]))
+
+X = iris["data"][:, (2, 3)] # petal length, petal width
+y = iris["target"]
+softmax_reg = LogisticRegression(multi_class="multinomial",solver="lbfgs", C=10)
+softmax_reg.fit(X, y)
+
+print(softmax_reg.predict([[5, 2]]))
+print(softmax_reg.predict_proba([[5, 2]]))
