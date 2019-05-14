a3a99976b6f7fa06df39b01734acca0d474a0daf
aCuissot <harrypotter9752@gmail.com>
2019-05-14 11:06:08 +0200

    subclassing model

---
+++ Keras/3_About_model/1_Model_subclassing.py
@@ -0,0 +1,38 @@
+import keras
+import numpy as np
+
+x_train = np.random.random((1000, 20))
+y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
+x_test = np.random.random((100, 20))
+y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+
+
+class SimpleMLP(keras.Model):
+
+    def __init__(self, use_bn=False, use_dp=False, num_classes=10):
+        super(SimpleMLP, self).__init__(name='mlp')
+        self.use_bn = use_bn
+        self.use_dp = use_dp
+        self.num_classes = num_classes
+
+        self.dense1 = keras.layers.Dense(32, activation='relu')
+        self.dense2 = keras.layers.Dense(num_classes, activation='softmax')
+        if self.use_dp:
+            self.dp = keras.layers.Dropout(0.5)
+        if self.use_bn:
+            self.bn = keras.layers.BatchNormalization(axis=-1)
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        if self.use_dp:
+            x = self.dp(x)
+        if self.use_bn:
+            x = self.bn(x)
+        return self.dense2(x)
+
+
+model = SimpleMLP()
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=20, batch_size=128)
+score = model.evaluate(x_test, y_test, batch_size=128)
+print(score)
