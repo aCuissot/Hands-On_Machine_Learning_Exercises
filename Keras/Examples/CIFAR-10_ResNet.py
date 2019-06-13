3be2bc4d22740a5666c83705d41481add594360c
aCuissot <harrypotter9752@gmail.com>
2019-06-13 10:35:33 +0200

    minor changes

--- Keras/Examples/CIFAR-10_ResNet.py
+++ Keras/Examples/CIFAR-10_ResNet.py
@@ -1,3 +1,32 @@
+"""
+#Trains a ResNet on the CIFAR10 dataset.
+ResNet v1:
+[Deep Residual Learning for Image Recognition
+](https://arxiv.org/pdf/1512.03385.pdf)
+ResNet v2:
+[Identity Mappings in Deep Residual Networks
+](https://arxiv.org/pdf/1603.05027.pdf)
+Model|n|200-epoch accuracy|Original paper accuracy |sec/epoch GTX1080Ti
+:------------|--:|-------:|-----------------------:|---:
+ResNet20   v1|  3| 92.16 %|                 91.25 %|35
+ResNet32   v1|  5| 92.46 %|                 92.49 %|50
+ResNet44   v1|  7| 92.50 %|                 92.83 %|70
+ResNet56   v1|  9| 92.71 %|                 93.03 %|90
+ResNet110  v1| 18| 92.65 %|            93.39+-.16 %|165
+ResNet164  v1| 27|     - %|                 94.07 %|  -
+ResNet1001 v1|N/A|     - %|                 92.39 %|  -
+&nbsp;
+Model|n|200-epoch accuracy|Original paper accuracy |sec/epoch GTX1080Ti
+:------------|--:|-------:|-----------------------:|---:
+ResNet20   v2|  2|     - %|                     - %|---
+ResNet32   v2|N/A| NA    %|            NA         %| NA
+ResNet44   v2|N/A| NA    %|            NA         %| NA
+ResNet56   v2|  6| 93.01 %|            NA         %|100
+ResNet110  v2| 12| 93.15 %|            93.63      %|180
+ResNet164  v2| 18|     - %|            94.54      %|  -
+ResNet1001 v2|111|     - %|            95.08+-.14 %|  -
+"""
+
 from __future__ import print_function
 import keras
 from keras.layers import Dense, Conv2D, BatchNormalization, Activation
@@ -12,8 +41,6 @@ from keras.models import Model
 from keras.datasets import cifar10
 import numpy as np
 import os
-
-# Trains a ResNet on the CIFAR10 dataset.
 
 # Training parameters
 batch_size = 32  # orig paper trained all networks with batch_size=128
@@ -81,13 +108,10 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 def lr_schedule(epoch):
     """Learning Rate Schedule
-
     Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
     Called automatically every epoch as part of callbacks during training.
-
     # Arguments
         epoch (int): The number of epochs
-
     # Returns
         lr (float32): learning rate
     """
@@ -112,7 +136,6 @@ def resnet_layer(inputs,
                  batch_normalization=True,
                  conv_first=True):
     """2D Convolution-Batch Normalization-Activation stack builder
-
     # Arguments
         inputs (tensor): input tensor from input image or previous layer
         num_filters (int): Conv2D number of filters
@@ -122,7 +145,6 @@ def resnet_layer(inputs,
         batch_normalization (bool): whether to include batch normalization
         conv_first (bool): conv-bn-activation (True) or
             bn-activation-conv (False)
-
     # Returns
         x (tensor): tensor as input to the next layer
     """
@@ -151,7 +173,6 @@ def resnet_layer(inputs,
 
 def resnet_v1(input_shape, depth, num_classes=10):
     """ResNet Version 1 Model builder [a]
-
     Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
     Last ReLU is after the shortcut connection.
     At the beginning of each stage, the feature map size is halved (downsampled)
@@ -168,12 +189,10 @@ def resnet_v1(input_shape, depth, num_classes=10):
     ResNet44 0.66M
     ResNet56 0.85M
     ResNet110 1.7M
-
     # Arguments
         input_shape (tensor): shape of input image tensor
         depth (int): number of core convolutional layers
         num_classes (int): number of classes (CIFAR10 has 10)
-
     # Returns
         model (Model): Keras model instance
     """
@@ -225,7 +244,6 @@ def resnet_v1(input_shape, depth, num_classes=10):
 
 def resnet_v2(input_shape, depth, num_classes=10):
     """ResNet Version 2 Model builder [b]
-
     Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
     bottleneck layer
     First shortcut connection per layer is 1 x 1 Conv2D.
@@ -239,12 +257,10 @@ def resnet_v2(input_shape, depth, num_classes=10):
     stage 0: 32x32,  64
     stage 1: 16x16, 128
     stage 2:  8x8,  256
-
     # Arguments
         input_shape (tensor): shape of input image tensor
         depth (int): number of core convolutional layers
         num_classes (int): number of classes (CIFAR10 has 10)
-
     # Returns
         model (Model): Keras model instance
     """
--- Keras/Testing_Models/Image_classification/VGG16.py
+++ Keras/Testing_Models/Image_classification/VGG16.py
@@ -2,12 +2,18 @@ from keras.applications.vgg16 import VGG16, decode_predictions, preprocess_input
 from keras.preprocessing import image
 import numpy as np
 import keras
+import cv2 as cv
 
 # Using VGG16 to extract features:
 
 model = VGG16(weights='imagenet')
 img_path = '../../../Data/in/snail-shell.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
+# using opencv instead of Pillow
+img = cv.imread(img_path)
+blank_image = np.zeros((224, 224, 3), np.uint8)
+img = cv.resize(img, (224, 224))
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+# img = image.load_img(img_path, target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
